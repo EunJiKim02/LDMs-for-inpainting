a15b7d194f23bf14bfb2b98aa88abd0ead2b6f4d
EunjiKim02 <now0104@knu.ac.kr>
2024-04-08 22:34:04 +0900

    file location change

--- inpaint.py
+++ inpaint.py
@@ -56,9 +56,9 @@ if __name__ == "__main__":
     images = [x.replace("_mask.png", ".png") for x in masks]
     print(f"Found {len(masks)} inputs.")
 
-    config = OmegaConf.load("models/ldm/inpainting_big/config.yaml")
+    config = OmegaConf.load("ldm/configs/inpainting/inpainting_big.yaml")
     model = instantiate_from_config(config.model)
-    model.load_state_dict(torch.load("models/ldm/inpainting_big/last.ckpt")["state_dict"],
+    model.load_state_dict(torch.load("ldm/last.ckpt")["state_dict"],
                           strict=False)
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
